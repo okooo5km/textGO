84f4dceee7b8f16adac903ee9b04e6c9146d39c5
5km <5km@smslit.cn>
2019-08-25 13:38:32 +0800

    优化更新功能

--- textGO/TextGoUpdater.swift
+++ textGO/TextGoUpdater.swift
@@ -12,14 +12,11 @@ import Cocoa
 class TextGoUpdater {
     
     private let url: URL?
-    private let user: String
     
-    static let share = TextGoUpdater(user: "smslit")
+    static let share = TextGoUpdater()
     
-    init(user: String) {
-        self.user = user
-        let proName = Bundle.main.infoDictionary!["CFBundleExecutable"]!
-        self.url = URL(string: "https://raw.githubusercontent.com/\(user)/\(proName)/master/\(proName)/Info.plist")
+    init() {
+        self.url = URL(string: "https://dev.tencent.com/u/smslit/p/appupdate/git/raw/master/textgo/macos.json")
     }
     
     func check(callback: @escaping (()->Void)) {
@@ -40,10 +37,9 @@ class TextGoUpdater {
                             withMessage: NSLocalizedString("check-update-tip-network.message", comment: "网络异常！"))
                     return
                 }
-                var propertyListForamt = PropertyListSerialization.PropertyListFormat.xml
                 do {
-                    let infoPlist = try PropertyListSerialization.propertyList(from: data!, options: PropertyListSerialization.ReadOptions.mutableContainersAndLeaves, format: &propertyListForamt) as! [String: AnyObject]
-                    let latestVersion = infoPlist["CFBundleShortVersionString"] as! String
+                    let infoJson = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                    let latestVersion = infoJson.value(forKey: "version") as! String
                     let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
                     if latestVersion == appVersion {
                         tipInfo(withTitle: NSLocalizedString("check-update-tip.title", comment: "检查更新"),
@@ -52,16 +48,16 @@ class TextGoUpdater {
                     }
                     
                     tipInfo(withTitle: NSLocalizedString("check-update-tip.title", comment: "检查更新"),
-                            withMessage: NSLocalizedString("check-update-tip-get.message", comment: "发现新版本") + " v\(latestVersion)",
+                            withMessage: NSLocalizedString("check-update-tip-get.message", comment: "发现新版本") + " v\(latestVersion)\n\n" + (infoJson.value(forKey: "log") as! String),
                         oKButtonTitle: NSLocalizedString("check-update-tip-get-gobutton.title", comment: "前往下载"),
                         cancelButtonTitle: NSLocalizedString("check-update-tip-get-ignorebutton.title", comment: "忽略")) {
-                            if let url = URL(string: "https://github.com/\(self.user)/\(Bundle.main.infoDictionary!["CFBundleExecutable"]!)/releases/tag/v" + latestVersion) {
+                            if let url = URL(string: infoJson.value(forKey: "url") as! String) {
                                 NSWorkspace.shared.open(url)
                             }
                     }
                 } catch {
                     // :TODO 加日志
-                    print("Error reading plist: \(error), format: \(propertyListForamt)")
+                    print("Error reading info: \(error)")
                 }
             }
         }
